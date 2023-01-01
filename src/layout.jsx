d3669cebc6e66e3383f3ebf41df04c643c67b350
Hafizur Rahman Fahim <hafijurrahman046@gmail.com>
2023-01-01 17:39:28 +0600

    add custom theme

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,10 +1,49 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Container } from "@mui/system";
 import Navigation from "./components/navigation";
 
+// Define the styles
+const NavigationActionRoot = {
+  paddingBottom: 20,
+  "&.Mui-selected": {
+    color: "white",
+  },
+  "& .MuiSvgIcon-root": {
+    padding: "5px 15px 5px 15px",
+    margin: 10,
+    borderRadius: 15,
+  },
+  "&.Mui-selected .MuiSvgIcon-root": {
+    background: "#E4B454",
+  },
+};
+const BottomNavigationRoot = {
+  height: 100,
+  position: "fixed",
+  bottom: 0,
+  width: "100%",
+  background: "rgba(0, 0, 0, 0.56)",
+};
+
 export default function Layout(props) {
+  const theme = createTheme({
+    components: {
+      MuiBottomNavigationAction: {
+        styleOverrides: { root: NavigationActionRoot },
+      },
+      MuiBottomNavigation: {
+        styleOverrides: { root: BottomNavigationRoot },
+      },
+    },
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
-    <div>
-      {props.children}
+    <ThemeProvider theme={theme}>
       <Navigation />
-    </div>
+      <Container>{props.children}</Container>
+    </ThemeProvider>
   );
 }
