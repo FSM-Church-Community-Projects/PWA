f5047b13139a2f46caee560869919b44fce471f9
Hafizur Rahman Fahim <hafijurrahman046@gmail.com>
2023-01-02 00:10:15 +0600

    add static contents to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,17 @@
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import Button from "@mui/material/Button";
+
+const ButtonSX = {
+  borderRadius: 15,
+  background: "#E4B454",
+};
+const alignYCenter = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+};
 
 export default function Home() {
   return (
@@ -10,9 +23,80 @@ export default function Home() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
+          marginBottom: "40px",
         }}
       >
         <img alt="logo" className="headerImg" src="/header.png" />
+      </Grid>
+      <Grid item xs={12} marginBottom="">
+        <Typography
+          variant="arrow"
+          component="h1"
+          sx={{
+            marginBottom: "25px",
+          }}
+        >
+          Today
+        </Typography>
+        <Typography variant="regular" component="h2">
+          <WbSunnyOutlinedIcon
+            sx={{
+              fontSize: "1.5rem",
+              marginRight: "10px",
+            }}
+          />
+          20°
+        </Typography>
+        <Typography variant="regular" component="h2">
+          12:06AM, 2 JANUARY 2023
+        </Typography>
+        <Typography variant="regular" component="h2">
+          Happy{" "}
+          <span
+            style={{
+              color: "#E4B454",
+              textDecoration: "underline",
+              marginLeft: "5px",
+            }}
+          >
+            Saturnalia and Solstice!
+          </span>
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography
+          variant="arrow"
+          component="h1"
+          sx={{
+            marginBottom: "10px",
+          }}
+        >
+          News
+        </Typography>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item xs={9}>
+            <Typography variant="regular" component="h2">
+              1. Mr Pasta gained right to wear colander on his NID photo
+              Installing FSM app makes you 200% more cool legally
+            </Typography>
+          </Grid>
+          <Grid item xs={2} sx={alignYCenter}>
+            <Button variant="contained" sx={ButtonSX}>
+              READ
+            </Button>
+          </Grid>
+          <Grid item xs={9}>
+            <Typography variant="regular" component="h2">
+              2. Installing FSM app makes you 200% more cool legally
+            </Typography>
+          </Grid>
+          <Grid item xs={2} sx={alignYCenter}>
+            <Button variant="contained" sx={ButtonSX}>
+              READ
+            </Button>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
