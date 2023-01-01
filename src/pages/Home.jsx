deec8660fab5ca6e00325c93d8677a21606146af
Hafizur Rahman Fahim <hafijurrahman046@gmail.com>
2023-01-01 22:00:29 +0600

    added header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import Grid from "@mui/material/Grid";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Obey your noodly master!</h1>
-    </div>
+    <Grid container spacing={2} justifyContent="center" alignItems="center">
+      <Grid
+        item
+        xs={12}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img className="headerImg" src="/header.png" />
+      </Grid>
+    </Grid>
   );
 }
